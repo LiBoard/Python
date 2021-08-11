f320f64fa2003ea4db599bc1608442857e667d62
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2021-08-11 14:58:20 +0200

    Update copyright

--- liboard/__init__.py
+++ liboard/__init__.py
@@ -1,10 +1,3 @@
-# Copyright (C) 2021  Philipp Leclercq
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
 """
 Interact with LiBoard-type electronic chessboards.
 
@@ -12,11 +5,27 @@ Classes:
     LiBoard
 """
 
-import chess
-from serial import Serial
-from bitstring import Bits
+#  LiBoard
+#  Copyright (C) 2021 Philipp Leclercq
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License version 3 as published by
+#  the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from time import time_ns
 from typing import Callable, Optional
+
+import chess
+from bitstring import Bits
+from serial import Serial
 
 
 class LiBoard:
