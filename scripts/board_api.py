a6dfc948e14178a9f8fb7105e4ab04f755144893
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2021-11-19 23:44:12 +0100

    Put ticks in separate thread

--- scripts/board_api.py
+++ scripts/board_api.py
@@ -21,7 +21,7 @@ import argparse
 import json
 import logging
 from concurrent.futures import ThreadPoolExecutor
-from queue import Empty, Queue
+from queue import Queue
 from time import sleep
 
 import requests
@@ -32,6 +32,13 @@ from liboard.move_recognition import BoardApiMoveRecognizer
 from liboard.physical import USBBoard
 
 
+def _init_logging(args):
+    if args.debug.lower() in {'info', 'i'}:
+        logging.basicConfig(level=logging.INFO)
+    elif args.debug.lower() in {'debug', 'd'}:
+        logging.basicConfig(level=logging.DEBUG)
+
+
 def _stream(url, headers):
     stream = requests.get(url, headers=headers, stream=True)
     if stream.encoding is None:
@@ -39,31 +46,36 @@ def _stream(url, headers):
     yield from (json.loads(item) for item in stream.iter_lines(decode_unicode=True) if item)
 
 
-def _main(args: argparse.Namespace):
-    if args.debug.lower() in {'info', 'i'}:
-        logging.basicConfig(level=logging.INFO)
-    elif args.debug.lower() in {'debug', 'd'}:
-        logging.basicConfig(level=logging.DEBUG)
+def _watch_events(queue, headers):
+    for _item in _stream('https://lichess.org/api/stream/event', headers):
+        logging.debug(_item)
+        queue.put(_item)
 
+
+def _watch_moves(queue, headers, game_id):
+    for _item in _stream(f'https://lichess.org/api/board/game/stream/{game_id}', headers):
+        logging.debug(_item)
+        state = _item if _item['type'] == 'gameState' \
+            else _item['state'] if _item['type'] == 'gameFull' else None
+        if state:
+            if state['status'] != 'started':
+                logging.info(f"Status: {state['status']}")
+                return
+            queue.put(state['moves'])
+
+
+def _tick(*args, delay=0):
+    while True:
+        for t in args:
+            t.tick()
+        sleep(delay)
+
+
+def _main(args: argparse.Namespace):
+    _init_logging(args)
     headers = {'Authorization': f'Bearer {args.token}'}
     queue = Queue()
     game_id = ''
-
-    def _watch_events():
-        for _item in _stream('https://lichess.org/api/stream/event', headers):
-            logging.debug(_item)
-            queue.put(_item)
-
-    def _watch_moves():
-        for _item in _stream(f'https://lichess.org/api/board/game/stream/{game_id}', headers):
-            logging.debug(_item)
-            state = _item if _item['type'] == 'gameState' \
-                else _item['state'] if _item['type'] == 'gameFull' else None
-            if state:
-                if state['status'] != 'started':
-                    logging.info(f"Status: {state['status']}")
-                    return
-                queue.put(state['moves'])
 
     def _callback(board: Board):
         if board.ply:
@@ -73,38 +85,33 @@ def _main(args: argparse.Namespace):
                 f'https://lichess.org/api/board/game/{game_id}/move/{move}',
                 headers=headers)
 
-    with ThreadPoolExecutor(max_workers=3) as executor:
-        logging.debug('Starting _watch_events')
-        executor.submit(_watch_events)
+    recognizer = BoardApiMoveRecognizer(_callback, args.move_delay)
+    usb_board = USBBoard(recognizer.on_event, args.port, args.baud_rate)
 
-        recognizer = BoardApiMoveRecognizer(_callback, args.move_delay)
-        usb_board = USBBoard(recognizer.on_event, args.port, args.baud_rate)
-        logging.debug('Connecting to board')
-        with usb_board.connect():
-            while True:
-                try:
-                    item = queue.get_nowait()
-                    if isinstance(item, dict):
-                        if item['type'] == 'gameStart':
-                            if item['game']['compat']['board']:
-                                game_id = item['game']['id']
-                                logging.info(f'Game {game_id} started')
-                                executor.submit(_watch_moves)
-                            else:
-                                logging.warning(
-                                    f"Board incompatible game {item['game']['id']}")
-                        elif item['type'] == 'gameFinish':
-                            logging.debug(f"gameFinish {item['game']['id']}")
-                            if item['game']['id'] == game_id:
-                                logging.debug(f'Game {game_id} finished')
-                                game_id = ''
-                    elif isinstance(item, str):
-                        recognizer.handle_streamed_moves(item)
-                except Empty:
-                    pass
-                usb_board.tick()
-                recognizer.tick()
-                sleep(args.move_delay / 10000)  # helps reducing CPU load
+    logging.debug('Connecting to board')
+    with ThreadPoolExecutor() as executor, usb_board.connect():
+        logging.debug('Starting _watch_events')
+        executor.submit(_watch_events, queue, headers)
+        logging.debug('Starting ticks')
+        executor.submit(_tick, usb_board, recognizer, delay=args.move_delay / 5000)
+        while True:
+            item = queue.get()
+            if isinstance(item, dict):
+                if item['type'] == 'gameStart':
+                    if item['game']['compat']['board']:
+                        game_id = item['game']['id']
+                        logging.info(f'Game {game_id} started')
+                        executor.submit(_watch_moves, queue, headers, game_id)
+                    else:
+                        logging.warning(
+                            f"Board incompatible game {item['game']['id']}")
+                elif item['type'] == 'gameFinish':
+                    logging.debug(f"gameFinish {item['game']['id']}")
+                    if item['game']['id'] == game_id:
+                        logging.debug(f'Game {game_id} finished')
+                        game_id = ''
+            elif isinstance(item, str):
+                recognizer.handle_streamed_moves(item)
 
 
 if __name__ == '__main__':
